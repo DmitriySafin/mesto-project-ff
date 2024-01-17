bb8b85d2c1f3d404b32bbc54ba9c78a55a1b2cd7
Дмитрий Сафин <waystore24@gmail.com>
2024-01-17 20:05:13 +0300

    Исправил замечания на ревью

--- scripts/index.js
+++ scripts/index.js
@@ -1,20 +1,34 @@
 // @todo: Темплейт карточки
 const cardTemplate = document.querySelector("#card-template").content;
+
 // @todo: DOM узлы
 const placeList = document.querySelector(".places__list");
+const placeItem = document.querySelector(".places__item");
+
 // @todo: Функция создания карточки
-initialCards.forEach(function (item) {
+const createCard = (cardData, deleteCallback) => {
   const cardElement = cardTemplate
     .querySelector(".places__item")
     .cloneNode(true);
-  cardElement.querySelector(".card__image").src = item.link;
-  cardElement.querySelector(".card__title").textContent = item.name;
-  placeList.append(cardElement);
-});
-// @todo: Функция удаления карточки
-document.querySelector(".places").addEventListener("click", (evt) => {
-  console.log(evt.target);
-  if (evt.target.className != "card__delete-button") return;
-  let item = evt.target.closest(".places__item");
-  item.remove();
-});
+  cardElement.querySelector(".card__image").src = cardData.link;
+  cardElement.querySelector(".card__image").alt = cardData.name;
+  cardElement.querySelector(".card__title").textContent = cardData.name;
+  const deletBtn = cardElement.querySelector(".card__delete-button");
+  deletBtn.addEventListener("click", deleteCallback);
+  return cardElement;
+};
+
+// // @todo: Функция удаления карточки
+function deletCard(evt) {
+  const delElem = evt.target.closest(".places__item");
+  delElem.remove(placeItem);
+}
+// // @todo: Функция показа карточки
+function showCard(card) {
+  card.forEach((item) => {
+    const call = createCard(item, deletCard);
+    placeList.append(call);
+  });
+}
+
+showCard(initialCards);
